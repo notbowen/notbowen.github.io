ce8b84d65cc4ed7973f484865a5a9e5fa7b56740
wHo69 <hu_bowen@students.edu.sg>
2022-09-10 20:02:11 +0800

    Added O Lvl Countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,30 @@
+let day = document.querySelector('.days')
+let hour = document.querySelector('.hours')
+let minute = document.querySelector('.minutes')
+let second = document.querySelector('.seconds')
+let mainBox = document.querySelector('.main_box')
+
+setInterval(function(){
+
+    let UpcomingDate = new Date('14 October 2022 12:00:00 AM');
+    let NowDate = new Date();
+    let differanceTime  = Math.floor((UpcomingDate-NowDate)/1000)
+    
+    
+    let differanceDay = Math.floor(differanceTime/60/60/24)
+    let differanceHour = Math.floor((differanceTime/60/60)%24)
+    let differanceMinute = Math.floor((differanceTime/60)%60)
+    let differanceSecound = Math.floor(differanceTime%60)
+    
+    
+    
+    if(differanceDay >= 0){
+        mainBox.style.display = "block";
+        day.innerHTML =  differanceDay;
+        hour.innerHTML =  differanceHour;
+        minute.innerHTML =differanceMinute;
+        second.innerHTML = differanceSecound;   
+    }
+
+
+},1000)
